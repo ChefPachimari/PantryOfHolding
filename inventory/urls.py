a723e6829434ebff632e822e0c3b7ebfc4ba494f
ChefPachimari <bboyteddybear@gmail.com>
2024-11-18 10:48:01 -0500

    add permission control

--- inventory/urls.py
+++ inventory/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import PantryViewSet, FoodCategoryViewSet, FoodViewSet
 
 router = DefaultRouter()
-router.register(r'users/(?P<user_id>\d+)/pantry', PantryViewSet, basename='pantry')
+router.register(r'pantry', PantryViewSet, basename='pantry')
 router.register(r'food-categories', FoodCategoryViewSet, basename='food-category')
 router.register(r'foods', FoodViewSet, basename='food')
 
--- inventory/views.py
+++ inventory/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseForbidden
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import Pantry, FoodCategory, Food
 from .serializers import PantrySerializer, FoodCategorySerializer, FoodSerializer
 
@@ -8,15 +10,17 @@ class PantryViewSet(viewsets.ModelViewSet):
     A viewset for viewing and editing user's pantry instances.
     """
     serializer_class = PantrySerializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        user_id = self.kwargs['user_id']
-        return Pantry.objects.filter(user__id=user_id)
+        print(self.request.user)
+        return Pantry.objects.filter(user=self.request.user)
     
 class FoodCategoryViewSet(viewsets.ModelViewSet):
     serializer_class = FoodCategorySerializer
     queryset = FoodCategory.objects.all()
 
 class FoodViewSet(viewsets.ModelViewSet):
+    permission_classes = [AllowAny]
     serializer_class = FoodSerializer
     queryset = Food.objects.all()
